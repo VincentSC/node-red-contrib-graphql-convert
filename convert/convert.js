ca465b98c4d0e2e194193b7babad468d63f351a8
Renze Nicolai <renze@rnplus.nl>
2018-11-06 17:49:22 +0100

    Remove debug messages

--- convert/convert.js
+++ convert/convert.js
@@ -7,13 +7,9 @@ module.exports = function(RED) {
         var node = this;
 
         this.on('input', function (msg) {
-            node.warn("I saw a payload: "+msg.payload+" ("+typeof msg.payload+")");
-			
 			if (this.convertTo === "string")  msg.payload = String(msg.payload);
 			if (this.convertTo === "number")  msg.payload = Number(msg.payload);
 			if (this.convertTo === "boolean") msg.payload = Boolean(msg.payload);
-			
-			node.warn("... and I converted it to "+msg.payload+" ("+typeof msg.payload+")");
             node.send(msg);
         });
     }
