4aa5a53d20c3d3ef03d0bb59668cbfe7851d8b03
Vincent <vincent@streamhpc.com>
2024-02-14 16:50:04 +0100

    WIP

--- convert/graphql2json.js
+++ convert/graphql2json.js
@@ -1,13 +1,19 @@
-const {graphql2Json} = require("graphql-query-to-json")
+const {convertGraphql2Json} = require("graphql-query-to-json")
 
 module.exports = function(RED) {
-    "use strict";
 
     function Graphql2JsonConverter(n) {
         RED.nodes.createNode(this,n);
- 
-        });
+        let node = this;
+		this.on('input', function(msg, send, done) {
+			send = send || function() { node.send.apply(node,arguments) }
+			msg.graphql = convertGraphql2Json(msg.json, { pretty: true });
+			send(msg);
+		
+			if (done) {
+				done();
+			}
     }
 
-    RED.nodes.registerType("graphql2json",Graphql2JsonConverter);
+    RED.nodes.registerType("graphql2json", Graphql2JsonConverter);
 }
--- convert/json2graphql.js
+++ convert/json2graphql.js
@@ -1,13 +1,20 @@
-const {json2Graphql} = require("json-to-graphql-query")
+const {convertJson2Graphql} = require("json-to-graphql-query")
 
 module.exports = function(RED) {
-    "use strict";
 
     function Json2GraphqlConverter(n) {
         RED.nodes.createNode(this,n);
- 
-        });
+		let node = this;
+		this.on('input', function(msg, send, done) {
+			send = send || function() { node.send.apply(node,arguments) }
+			msg.graphql = convertJson2Graphql(msg.json, { pretty: true });
+			send(msg);
+		
+			if (done) {
+				done();
+			}
+		});
     }
 
-    RED.nodes.registerType("json2graphql",Json2GraphqlConverter);
+    RED.nodes.registerType("json2graphql", Json2GraphqlConverter);
 }
